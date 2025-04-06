47df7b9575fccf298c39ce33e2920835971545ea
karxerOa <ortizacevedodulcemaria@gmail.com>
2025-04-06 18:35:50 -0500

    Repositorioejm.java

--- src/repositorioejm/Repositorioejm.java
+++ src/repositorioejm/Repositorioejm.java
@@ -16,6 +16,9 @@ public class Repositorioejm {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola mundo xd");
+        System.out.println("Hola mundo xd");
+        System.out.println("Hola mundo xd");
+        System.out.println("Hola mundo xd");
     }
     
 }
